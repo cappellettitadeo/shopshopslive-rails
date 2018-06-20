f2db54fc04a9789c648db147d2eb4ea3b13a420c
cappellettitadeo <cappellettitadeo@gmail.com>
2018-06-20 19:53:05 +0800

    Update product sync worker

--- app/models/callback_setting.rb
+++ app/models/callback_setting.rb
@@ -1,3 +1,11 @@
 class CallbackSetting < ApplicationRecord
   scope :products, -> { where(callback_type: 'product') }
+
+  def bunch_update?
+    mode == 'bunch'
+  end
+
+  def immediate_update?
+    mode == 'immediate'
+  end
 end
--- app/workers/products_sync_worker.rb
+++ app/workers/products_sync_worker.rb
@@ -4,12 +4,19 @@ class ProductsSyncWorker
   sidekiq_options unique: true
 
   def perform
-    SyncQueue.products.find_in_batches(batch_size: 20) do |items|
+    setting = CallbackSetting.products.first
+    # 如果mode不是"bunch"，则直接返回
+    return unless setting && setting.bunch_update?
+
+    SyncQueue.products.find_in_batches(batch_size: setting.bunch_size) do |items|
       products = items.collect(:target)
       products_hash = ProductSerializer.new(products).serializable_hash
-      setting = CallbackSetting.products.first
       # POST to Center System
       if setting
+        url = setting.url
+        body = { products: products_hash }
+        res = HTTParty.post(url, { body: body })
+        return false if res.code != 200
       end
     end
   end
