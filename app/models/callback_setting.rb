11cfeb87adb22e9a701c1c16ff2d2b308801203d
cappellettitadeo <cappellettitadeo@gmail.com>
2018-07-10 17:35:01 -0700

    refactored CallbackSetting.sync_with_central_app

--- app/models/callback_setting.rb
+++ app/models/callback_setting.rb
@@ -3,6 +3,26 @@ class CallbackSetting < ApplicationRecord
   scope :stores, -> { where(callback_type: 'store') }
   scope :vendor, -> { where(callback_type: 'vendor') }
 
+  def self.sync_with_central_app
+    parsed_json = CentralApp::Utils::Callback.list_all
+
+    if parsed_json
+      parsed_json.each do |key, value|
+        url = value[:callback].strip rescue nil
+        mode = value[:mode].strip
+        bunch_size = value[:bunchsize]
+        return if url.empty?
+
+        setting = CallbackSetting.where(callback_type: key.downcase).first_or_create
+        # 更新CallbackSetting设置
+        setting.url = url
+        setting.mode = mode if mode
+        setting.bunch_size = bunch_size if bunch_size
+        setting.save
+      end
+    end
+  end
+
   def bunch_update?
     mode == 'bunch'
   end
--- app/workers/callback_settings_sync_worker.rb
+++ app/workers/callback_settings_sync_worker.rb
@@ -6,26 +6,7 @@ class CallbackSettingsSyncWorker
 
   sidekiq_options unique: true
 
-  # TODO
-  SETTING_URL = ''
-
   def perform
-    parsed_json = CentralApp::Utils::Callback.list_all
-
-    if parsed_json
-      parsed_json.each do |key, value|
-        url = value[:callback].strip rescue nil
-        mode = value[:mode].strip
-        bunch_size = value[:bunchsize]
-        return if url.empty?
-
-        setting = CallbackSetting.where(callback_type: key.downcase).first_or_create
-        # 更新CallbackSetting设置
-        setting.url = url
-        setting.mode = mode if mode
-        setting.bunch_size = bunch_size if bunch_size
-        setting.save
-      end
-    end
+    CallbackSetting.sync_with_central_app
   end
 end
