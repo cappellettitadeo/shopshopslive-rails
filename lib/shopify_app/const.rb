9eca783563921a012a0395a36e33ba580a3456ca
cappellettitadeo <cappellettitadeo@gmail.com>
2018-07-18 11:38:17 -0700

    solved a 'already initialized constant' warning

--- lib/shopify_app/const.rb
+++ lib/shopify_app/const.rb
@@ -1,11 +1,11 @@
 module ShopifyApp
   class Const
-    API_KEY = ENV['SHOPIFY_API_KEY']
-    API_SECRET = ENV['SHOPIFY_API_SECRET']
-    BASE_URL = ENV['BASE_URL']
-    APP_URL = "#{BASE_URL}/shopify_app"
-    SCOPE = %w(read_product_listings write_draft_orders)
-    EVENTS_TOPICS = {
+    API_KEY ||= ENV['SHOPIFY_API_KEY']
+    API_SECRET ||= ENV['SHOPIFY_API_SECRET']
+    BASE_URL ||= ENV['BASE_URL']
+    APP_URL ||= "#{BASE_URL}/shopify_app"
+    SCOPE ||= %w(read_product_listings write_draft_orders)
+    EVENTS_TOPICS ||= {
         :product_listings => %w(add remove update),
         :shop => %w(update),
         :app => %w(uninstalled)}
