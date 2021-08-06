201e8c8bfd95aaa72aa7945a014cda5dd9a745a6
cappellettitadeo <cappellettitadeo@gmail.com>
2021-08-06 13:02:55 +0800

    default to variant.price

--- app/models/scrapers/shopify/result_variant.rb
+++ app/models/scrapers/shopify/result_variant.rb
@@ -77,7 +77,7 @@ class Scrapers::Shopify::ResultVariant < Scrapers::Result
   end
 
   def original_price
-    @original_price = variant.compare_at_price
+    @original_price = variant.compare_at_price || variant.price
   end
 
   def product_id
