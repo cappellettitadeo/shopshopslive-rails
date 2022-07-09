0b2179049d04ebcb00e0dc849ddc9fd484cd639e
cappellettitadeo <cappellettitadeo@gmail.com>
2022-07-09 19:10:43 +0500

    set headers in api_controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,38 @@
 class ApiController < ActionController::API
+  before_action :set_headers
   before_action :authenticate_request
 
   private
 
-  def authenticate_request
-    headers = request.headers
-    puts "---- headers: #{headers} -----"
-    authorization_headers = headers['Authorization']
-    puts "---- athorization_headers: #{authorization_headers} -----"
+  def set_headers
+    @headers = request.headers
+  end
 
-    @api_key = AuthorizeApiRequest.call(authorization_headers).result
+  def authenticate_request
+    @api_key = api_key
+
     render json: { error: 'Not Authorized' }, status: 401 unless @api_key
+  end
+
+  def api_key
+    @api_key ||= ApiKey.find_by_key(decoded_auth_token[:api_key]) if decoded_auth_token
+    puts '@api_key'
+    puts @api_key
+    @api_key || errors.add(:token, 'Invalid token') && nil
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+
+    @decoded_auth_token
+  end
+
+  def http_auth_header
+    if @headers['Authorization'].present?
+      return @headers['Authorization'].split(' ').last
+    else
+      errors.add(:token, 'Missing token')
+    end
+    nil
   end
 end
